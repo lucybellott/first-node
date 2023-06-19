d3820d207562ac8f7a7d10a8b6da9fa707da1573
Lucy <lucybellott@gmail.com>
2023-06-19 18:12:50 -0400

    api

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
-app.get('/', (req,res) => {
+const jsonObj = {
+    'name': 'Lucy',
+    'city': 'new york'
+}
+
+app.get('/', (req, response)=>{
     response.sendFile(__dirname + '/index.html')
 })
 
-app.listen(PORT, () => {
+app.get('/api/', (req, response)=>{
+    response.json(jsonObj)
+})
+
+app.listen(PORT, ()=>{
     console.log(`the server is running on ${PORT}`)
 })
