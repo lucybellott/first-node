8bb6903f7e69f1fe2bdceadd27da7529427eef88
Lucy <lucybellott@gmail.com>
2023-06-19 17:48:43 -0400

    express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require('express')
+const app = express()
+const PORT = 8000
+
+app.get('/', (req,res) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`the server is running on ${PORT}`)
+})
