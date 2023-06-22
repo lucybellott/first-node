77ffe36c8e073c24c912ae7a322406f1e875f8c5
Lucy <lucybellott@gmail.com>
2023-06-22 14:56:22 -0400

    client side

--- client-js/main.js
+++ client-js/main.js
@@ -1,5 +1,12 @@
 async function apiRequest() {
 
-const personInfo = document.querySelector('input').value
+const personNumber = document.querySelector('input').value
+try {
+    const response = await fetch(`https://first-node-project-1b1c8312afb3.herokuapp.com/api/${personNumber}`)
+    const data = await response.json()
+    console.log(data)
+}catch(error) {
+    console.log(error)
+}
 
 }
